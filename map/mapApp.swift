09972cf5976d5717cac2e04f0acc00d58b95d0ad
zhunhe <xuzhunhe@gmail.com>
2022-09-19 20:40:47 +0900

    Initial Commit

---
+++ map/mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapApp.swift
+//  map
+//
+//  Created by 허준혁 on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
